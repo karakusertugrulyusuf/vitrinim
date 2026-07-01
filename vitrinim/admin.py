0426524b18d5efb78c9aef2b0d723445b3318543
karakusertugrulyusuf <karakusertugrulyusuf@gmail.com>
2026-07-01 16:02:54 +0300

    Update admin panel

--- vitrinim/admin.py
+++ vitrinim/admin.py
@@ -1,7 +1,99 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Product, Category, Cart, CartItem
 
-# Register your models here.
-from django.contrib import admin
-from .models import Product
 
-admin.site.register(Product)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "is_seller",
+        "is_approved",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+    )
+
+    list_filter = (
+        "is_seller",
+        "is_approved",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    ordering = ("-date_joined",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "seller",
+        "price",
+        "stock",
+        "created_at",
+    )
+
+    list_filter = (
+        "seller",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+        "seller__username",
+    )
+
+    ordering = ("-created_at",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "slug",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+    )
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "cart",
+        "product",
+        "quantity",
+    )
+
+    search_fields = (
+        "product__name",
+        "cart__user__username",
+    )
